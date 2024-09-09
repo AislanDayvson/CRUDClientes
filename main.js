68e8a27c991868dcf6093ff6cc596b2b9f6ccf05
Aislan <aislandnjunior@gmail.com>
2024-09-09 17:16:08 -0300

    ✨ Função CREATE para criar os clientes e por no localstorage

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () => document.getElementById('modal').classList.remove('active')
 
+const tempClient = {
+    nome: "Leo",
+    email: "aislan@gmail.com",
+    celular: "984583454",
+    cidade: "Recife"
+}
+
+const getLocalstorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalstorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
 // CRUD - CREATE / READ / UPDATE / DELETE
 
+// CRUD => CREATE
+const createClient = (client) => {
+    const db_client = getLocalstorage()
+    db_client.push(client)
+    setLocalstorage(db_client)
+}
 
 // Eventos
 document.getElementById('cadastrarCliente').addEventListener('click', openModal)
