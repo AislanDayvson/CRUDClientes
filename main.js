d3b1ffbdeb6652a67ba7a9379040da5a60063fb9
Aislan <aislandnjunior@gmail.com>
2024-09-09 10:34:14 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const openModal = () => document.getElementById('modal').classList.add('active')
+const closeModal = () => document.getElementById('modal').classList.remove('active')
+
+// CRUD - CREATE / READ / UPDATE / DELETE
+
+
+// Eventos
+document.getElementById('cadastrarCliente').addEventListener('click', openModal)
+document.getElementById('modalClose').addEventListener('click', closeModal)
+
